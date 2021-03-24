7afd8863b14bc056ec51f2dd08dd0def80ba1172
drtdenis <drouetdenis2@gmail.com>
2021-03-24 10:52:44 +0100

    correction

--- sayMyName.js
+++ sayMyName.js
@@ -2,6 +2,9 @@ if (process.argv.length != 3) {
   console.log(`usage: node sayMyName.js name`)
   process.exit(1)
 }
+console.log(`My name is ${process.argv[2]}`)
 
-const name = process.argv[2]
-console.log(`My name is ${name}`)
+
+
+//const name = process.argv[2]
+//console.log(`My name is ${name}`)
